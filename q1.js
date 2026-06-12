8fe63cb14a843e439b6203349246feb445c2ab70
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-11 21:19:48 -0300

    adicionando questão 1

--- q1.js
+++ q1.js
@@ -1,8 +1,16 @@
-let num1 = 10;
-let num2 = 20;
-let num3 = 30;
+function mostrarResposta(){
 
-let mediaNumeros = (num1 + num2 + num3) / 3;
+    let num1 = 10;
+    let num2 = 20;
+    let num3 = 30;
 
-console.log("QUESTÃO 1");
-console.log("Média =", mediaNumeros);
+    let media = (num1 + num2 + num3) / 3;
+
+    document.getElementById("resultado").innerHTML = `
+        <p>Número 1: ${num1}</p>
+        <p>Número 2: ${num2}</p>
+        <p>Número 3: ${num3}</p>
+        <br>
+        <strong>Média: ${media}</strong>
+    `;
+}
