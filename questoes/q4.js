4bd32ffe276ebc5531f4219a4e982aba48342999
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-12 21:38:37 -0300

    atualizando

--- questoes/q4.js
+++ questoes/q4.js
@@ -1,17 +1,10 @@
 function mostrarResposta(){
 
-<<<<<<< HEAD
     let nome = document.getElementById("nome").value;
 
     let nota1 = Number(document.getElementById("nota1").value);
     let nota2 = Number(document.getElementById("nota2").value);
     let nota3 = Number(document.getElementById("nota3").value);
-=======
-    let nome = "João";
-    let nota1 = 7;
-    let nota2 = 8;
-    let nota3 = 6;
->>>>>>> 8e214e9c6ea527eb2a71fce098c07ab4856e1a9c
 
     let media = (nota1 + nota2 + nota3) / 3;
 
@@ -24,18 +17,8 @@ function mostrarResposta(){
     }
 
     document.getElementById("resultado").innerHTML = `
-<<<<<<< HEAD
         Aluno: ${nome} <br>
         Média: ${media.toFixed(2)} <br>
         Situação: ${situacao}
-=======
-        <p>Aluno: ${nome}</p>
-        <p>Nota 1: ${nota1}</p>
-        <p>Nota 2: ${nota2}</p>
-        <p>Nota 3: ${nota3}</p>
-        <br>
-        <p>Média: ${media}</p>
-        <strong>${situacao}</strong>
->>>>>>> 8e214e9c6ea527eb2a71fce098c07ab4856e1a9c
     `;
 }
--- questoes/q6.js
+++ questoes/q6.js
@@ -1,6 +1,5 @@
 function mostrarResposta(){
 
-<<<<<<< HEAD
     let nome = document.getElementById("nome").value;
 
     let peso = Number(
@@ -49,38 +48,5 @@ function mostrarResposta(){
         <p><strong>IMC:</strong> ${imc.toFixed(2)}</p>
 
         <p><strong>Classificação:</strong> ${classificacao}</p>
-=======
-    let nome = "Maria";
-    let peso = 70;
-    let altura = 1.70;
-
-    let imc = peso / (altura * altura);
-
-    let risco;
-
-    if(imc < 20){
-        risco = "Abaixo do peso";
-    }
-    else if(imc <= 25){
-        risco = "Normal";
-    }
-    else if(imc <= 30){
-        risco = "Excesso de peso";
-    }
-    else if(imc <= 35){
-        risco = "Obesidade";
-    }
-    else{
-        risco = "Obesidade mórbida";
-    }
-
-    document.getElementById("resultado").innerHTML = `
-        <p>Paciente: ${nome}</p>
-        <p>Peso: ${peso}kg</p>
-        <p>Altura: ${altura}m</p>
-        <br>
-        <p>IMC: ${imc.toFixed(2)}</p>
-        <strong>${risco}</strong>
->>>>>>> 8e214e9c6ea527eb2a71fce098c07ab4856e1a9c
     `;
 }
